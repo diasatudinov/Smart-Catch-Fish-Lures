cce1806e1fa3a8107cfe15404f805329d3c253b5
Dias <dias.atudinov@gmail.com>
2026-05-19 19:59:11 +0500

    setup

---
+++ Smart Catch Fish Lures/Smart Catch Fish Lures/Smart_Catch_Fish_LuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Catch_Fish_LuresApp.swift
+//  Smart Catch Fish Lures
+//
+//  Created by Dias Atudinov on 19.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Catch_Fish_LuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
